0ec8f9251493b229dfac0964fbaf7e02da1dca03
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-03-07 03:50:55 +0200

    install required packages

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+
+            $table->string('first_name');
+            $table->string('last_name');
+
+            $table->string('email', 255)->unique();
+            $table->string('phone', 15)->unique()->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->string('image')->nullable();
+            $table->date('birth_date')->nullable();
+
+            $table->string('role')->default('user');
+            $table->enum('status', ['active', 'deactive', 'panned'])->default('active');
+
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
+
             $table->string('password');
             $table->rememberToken();
+
             $table->timestamps();
         });
     }
